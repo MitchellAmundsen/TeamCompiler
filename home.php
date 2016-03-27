6f366d7018323f028983d17af166a8483bcf44e5
icazadori <dube.hj@gmail.com>
2016-03-26 20:27:30 -0700

    website

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="league.css">
+	<link rel="icon" type="image/png" href="images/favicon.png">
+</head>
+<body>
+	<div id="top">
+		<div id="logo">
+			<img src="images/logo.png" id="logoimage"/>
+			<div id="search">
+				<button id="searchbutton">Search</button>
+				<input id="playersearch" list="Search players...">
+			</div>
+		</div>
+		<div id="menu">
+			<div id="embossed">
+				<ul id="menuitems">
+					<li><a>Home</a></li>
+					<li><a>Champions</a></li>
+					<li><a>Profile</a></li>
+					<li><a>Teams</a></li>
+					<li><a>Account</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="spacer"></div>
+	</div>
+	<div id="mainpage"></div>
+</body>
+</html>
